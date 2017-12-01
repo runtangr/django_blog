650570fbd08304618fdccbe9ec14b005ef72bc20
tangrui <1048393815@qq.com>
2017-12-01 17:03:36 +0800

    modify site name by cover template.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,15 +3,7 @@ from .models import Question, Choice
 # Register your models here.
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ]
-
-admin.site.register(Question, QuestionAdmin)
-
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -22,5 +14,11 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
